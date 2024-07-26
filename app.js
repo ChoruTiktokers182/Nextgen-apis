8ec7daf1e6bc90c0cb1dec807b6cc561a0dfcc92
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2024-07-26 11:22:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', createProxyMiddleware({ target: '158.101.198.227:8643', changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: 'http://158.101.198.227:8643/', changeOrigin: true }));
 
 const server = http.createServer(app);
 server.listen(port, () => {
